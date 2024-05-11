2671d2d5f2806d7dbd8ab569bf254df7b396f533
nisann97 <143318090+nisann97@users.noreply.github.com>
2024-05-11 22:21:29 +0400

    Add files via upload

---
+++ HomeVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using test.Models;
+namespace test.ViewModels
+{
+	public class HomeVM
+	{
+		public List<Slider> Sliders { get; set; }
+		public SliderInfo SliderInfo { get; set; }
+		public List<Category> Categories { get; set; }
+		public List<Product> Products  { get; set; }
+		public Surprise Surprise { get; set; }
+		public List<SurpriseBulletPoints> SurpriseBulletPoints { get; set; }
+		public ExpertPanel ExpertPanel { get; set; }
+		public List<Expert> Experts { get; set; }
+	}
+}
+
